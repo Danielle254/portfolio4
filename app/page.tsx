84fd4c6877d9d1d126bd0879871a8809b62b30de
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-06-07 13:08:14 -0600

    style: configure custom fonts and color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <h1 className="text-3xl">Title</h1>
+      <Button>button</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="secondary">secondary</Button>
+      <p>some text</p>
+    </div>
+  );
 }
