32b0873d2db1b6f9c732fd53a5f5b932ed06eb43
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-06-23 15:51:15 -0600

    feat: add project list section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
+import ProjectList from "./components/ProjectList/ProjectList";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Title</h1>
-      <Button>button</Button>
-      <Button variant="outline">outline</Button>
-      <Button variant="secondary">secondary</Button>
-      <p>some text</p>
-    </div>
+    <>
+      <ProjectList />
+    </>
   );
 }
