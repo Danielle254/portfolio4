075a2fb12083dbb8a917e81eba975cfd2fcc5f1f
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-06-23 15:50:04 -0600

    feat: add header and footer sections to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Danielle Lindblom Portfolio",
-};
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function RootLayout({
   children,
@@ -12,7 +9,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="dark">{children}</body>
+      <body className="max-w-[1200px] mx-auto">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
