85c0e0774ab741427993091f7887824584467fe4
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-08-14 13:11:57 -0500

    style: add top border to footer for better visual delineation of sections

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="py-10">
+    <footer className="py-4 border-t-2 border-accent">
       <div className="max-w-6xl mx-auto flex flex-col items-start">
         <p className="text-primary text-base font-semibold mb-4">
           Danielle Lindblom - Frontend Software Developer
