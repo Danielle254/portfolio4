0f54c9498328bb3c9c7a319b7e8529dc4c8be938
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-08-14 13:12:41 -0500

    feat: create placeholder content for quantiiv project

--- app/projects/quantiiv/page.tsx
+++ app/projects/quantiiv/page.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { Globe, Code } from "lucide-react";
 
 export default function Quantiiv() {
-  return <div></div>;
+  return (
+    <div>
+      <title>Quantiiv Project | Danielle Lindblom Portfolio</title>
+      <h1 className="text-3xl my-4">Quantiiv</h1>
+      <Image
+        src="/projectSummary/quantiivCover.png"
+        alt="Quantiiv website"
+        sizes="100vw"
+        width={0}
+        height={0}
+        style={{
+          borderRadius: "5px",
+          border: "1px solid black",
+          width: "100%",
+          margin: "16px 0",
+          height: "auto",
+        }}
+      />
+      <section>
+        <p>description</p>
+      </section>
+      <section>
+        <h2 className="text-2xl my-8">Key Contributions</h2>
+        <ul className="list-disc list-inside">
+          <li>item</li>
+        </ul>
+      </section>
+      <div className="flex flex-row flex-wrap gap-8 mt-24 mb-12">
+        <Button asChild size="lg">
+          <Link href="" target="_blank">
+            Live Site <Globe />
+          </Link>
+        </Button>
+        <Button variant="outline" asChild size="lg">
+          <Link href="" target="_blank">
+            V2 Preview <Globe />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
