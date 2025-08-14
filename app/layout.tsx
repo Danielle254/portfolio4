7b4f5ad217833cdcac36cc7f860aab06e5f1f32c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-08-14 13:29:22 -0500

    style: add smooth scroll to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className="max-w-[1000px] mx-auto min-h-screen flex flex-col justify-between px-4">
         <Header />
         <main>{children}</main>
